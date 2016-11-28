b374bdc5cc7c138d90030c924ef5c28d2b931e47
abeler_wk@126.com <abeler_wk@126.com>
2016-11-29 00:15:43 +0800

    ''

---
+++ react_study/rec05/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import App from './components/App';
+import Home from './components/Home';
+import Repos from './components/Repos';
+import About from './components/About';
+import User from './components/User';
+import Contacts from './components/Contacts';
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="/repos/:name" component={Repos} />
+      <Route path="/about" component={About} />
+      <Route path="/user" component={User} />
+      <Route path="/contacts" component={Contacts} />
+    </Route>
+  </Router>,
+  document.getElementById('app'));
