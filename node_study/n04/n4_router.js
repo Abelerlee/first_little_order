5aa8214189fa81bb1b713538b9c7acbc60ff0a69
abeler_wk@126.com <abeler_wk@126.com>
2016-12-05 15:04:55 +0800

    ;;

---
+++ node_study/n04/n4_router.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var url = require('url');
+var router = require('./router');
+
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    if (request.url !== '/favicon.ico') {
+        // var routerName = url.parse(request.url).routerName;
+        // console.log(routerName);
+        // routerName = routerName.replace(/\//, '');
+        // router[routerName](request, response);
+        // response.end('');
+
+        var pathname = url.parse(request.url).pathname;
+        console.log(pathname);
+        pathname = pathname.replace(/\//, ''); //替换掉前面的/
+        //console.log(pathname);
+        router[pathname](request, response);
+        response.end('');
+    }
+}).listen(8000)
+
+console.log('Server running at http: //127.0.0.1:8000')
---
+++ node_study/n04/router.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    login: function(req, res) {
+        res.write('登录页面的模块')
+    },
+    regiser: function(req, res) {
+        res.write('注册页面的模块')
+    }
+}
